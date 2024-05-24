a770909e8505b951c3adac699b9639198485e365
M. de Vries <mdv840922163@gmail.com>
2024-05-24 19:39:23 +0200

    zsh

--- nvim/.config/nvim/lua/h4mm3r/core/keymaps.lua
+++ nvim/.config/nvim/lua/h4mm3r/core/keymaps.lua
@@ -1,10 +1,10 @@
 vim.g.mapleader = " "
 local keymap = vim.keymap -- for conciseness
 
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex, { desc = "Open netrw"})
+vim.keymap.set("n", "<leader>e", ":Ex<CR>", { desc = "Open netrw"})
 
 -- clear search highlights
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+vim.keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
