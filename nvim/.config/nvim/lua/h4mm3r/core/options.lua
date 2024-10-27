b9f5a5685fbd06cad3090b2021cff92d0dc66164
Marcel de Vries <marcel@de-vries.co.nl>
2024-10-27 14:54:41 +0100

    niet

--- nvim/.config/nvim/lua/h4mm3r/core/options.lua
+++ nvim/.config/nvim/lua/h4mm3r/core/options.lua
@@ -3,6 +3,7 @@ local opt = vim.opt -- for conciseness
 -- line numbers
 opt.relativenumber = true -- show relative line numbers
 opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+opt.colorcolumn = "80"
 
 -- tabs & indentation
 opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
@@ -28,6 +29,7 @@ opt.cursorcolumn = true
 opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
