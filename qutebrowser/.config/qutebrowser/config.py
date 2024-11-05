82ee2ece63d99bead8b26b78647c7798fed65437
Marcel de Vries <marcel@de-vries.co.nl>
2024-11-05 19:48:14 +0100

    whoopsies

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -145,7 +145,7 @@ config.bind(',sd', 'config-cycle content.user_stylesheets ~/.config/qutebrowser/
 config.bind(',sl', 'config-cycle content.user_stylesheets ~/.config/qutebrowser/solarized-everything-css/css/solarized-light/solarized-light-all-sites.css ""')
 
 c.colors.webpage.darkmode.enabled = True
-c.colors.webpage.darkmode.algorithm = 'lightness-hsl'
-c.colors.webpage.darkmode.contrast = -.022
-c.colors.webpage.darkmode.threshold.background = 100
-c.colors.webpage.darkmode.policy.images = 'always'
+#c.colors.webpage.darkmode.algorithm = 'lightness-hsl'
+#c.colors.webpage.darkmode.contrast = -.022
+#c.colors.webpage.darkmode.threshold.background = 100
+#c.colors.webpage.darkmode.policy.images = 'always'
