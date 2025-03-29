257820d28a99ad2a6a46913f1dda0279a84c4dff
pd3mdv <47981025+pd3mdv@users.noreply.github.com>
2025-03-29 14:03:18 +0100

    Update blocks.h

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -3,12 +3,11 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
   {"BAT:", "/usr/bin/cat /sys/class/power_supply/BAT0/capacity | awk '{print $1\"%\"}'",    5,    0},
   {"BAT:", "/usr/bin/cat /sys/class/power_supply/BAT1/capacity | awk '{print $1\"%\"}'",    5,    0},
-  {"AC:", "/usr/bin/cat /sys/class/power_supply/AC/online | awk '{print $1\"%\"}'",    5,    0},
-  {"wlan0:", "/usr/bin/cat /sys/class/net/wlan0/operstate | awk '{print $1}'",    5,    0},
-  {"enp0s31f6:", "/usr/bin/cat /sys/class/net/enp0s31f6/operstate | awk '{print $1}'",    5,    0},
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+  {"AC:",  "/usr/bin/cat /sys/class/power_supply/AC/online     | awk '{print $1}'"     ,    5,    0},
+  {"wlan0:", "/usr/bin/cat /sys/class/net/wlan0/operstate      | awk '{print $1}'"     ,    5,    0},
+  {"enp0s31f6:", "/usr/bin/cat /sys/class/net/enp0s31f6/operstate | awk '{print $1}'"  ,    5,    0},
+	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }'         | sed s/i//g"           ,	 30,		0},
+	{"", "date '+%b %d (%a) %I:%M%p'"                                                    ,		5,		0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
